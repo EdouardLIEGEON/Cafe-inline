31d7e3dcb20f043f3a99cfc18d1b738a62ccd887
elmaestro84 <46449730+elmaestro84@users.noreply.github.com>
2020-07-26 12:21:52 +0200

    Initial commit

---
+++ footer.php
@@ -0,0 +1,16 @@
+<!-- site-content -->
+<footer class="container-fluid">
+    <div id="footer_container" class="container">
+        <nav class="navbar d-flex justify-content-between" id="navfooter">
+            <a class="col-md-3 navbar-brand" target="_blank" href="https://www.accesscodeschool.fr/">ACCESS CODE SCHOOL</a>
+            <div class="col-md-4">
+            <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container_class' => 'footerMenu_class' ) ); ?>
+            </div>
+            <div class="col-md-4 d-flex justify-content-center">
+                <p>Thème créé par :<br>
+                Coralie, Sandra, Édouard, Aurélien</p>
+            </div>
+        </nav>
+    </div>
+</footer>
+</body>
---
+++ header.php
@@ -0,0 +1,22 @@
+
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <title>Café-inline</title>
+        <meta name="description" content="Blog de la promotion Café-inline">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Café-inline" />
+        <meta name="twitter:description" content="Le site de la promo Access Code School 2018/2019" />
+        <meta name="twitter:image" content="images/icons/logo_café-inline.png" />
+        <meta name="twitter:image:alt" content="logo Café-inline" /> 
+        <meta property="og:url" content="localhost/wordpress" />
+        <meta property="og:title" content="Café-inline" />
+        <meta property="og:description" content="Le site de la promo Access Code School 2018/2019" />
+        <meta property="og:image" content="images/icons/logo_café-inline.png" />
+        <meta property="og:image:alt" content="logo Café-inline" /> 
+        <?php wp_head(); ?>
+    </head>
+    <body>
+        
+        
